b0f97a1f485b5349681375a095ea134011d88e5d
iWw <i.1214@yahoo.com>
2021-04-23 14:09:27 +0800

    update namespace

---
+++ Sources/Segmenter/+SupplementaryContainerView.swift
@@ -0,0 +1,67 @@
+//
+//  Created by iWw on 2021/1/8.
+//
+
+import UIKit
+
+protocol SegmenterSupplementaryContainer { }
+
+public extension Segmenter {
+    
+    class SupplementaryContainerView: UIView, SegmenterSupplementaryContainer {
+        
+        public override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+            for subview in subviews.reversed() {
+                let isValidResponderView = !subview.isHidden && subview.alpha > 0 && subview.isUserInteractionEnabled
+                guard isValidResponderView else {
+                    continue
+                }
+                
+                if let subviewHitTest = subview.hitTest(point, with: event) {
+                    return subviewHitTest
+                }
+                if subview.frame.contains(point) {
+                    return subview
+                }
+                return super.hitTest(point, with: event)
+            }
+            return nil
+        }
+        
+        override public class var layerClass: AnyClass {
+            CAGradientLayer.self
+        }
+        
+        public func setColors(_ colors: [UIColor], startPoint: CGPoint = .init(x: 1, y: 0.5), endPoint: CGPoint = .init(x: 0, y: 0.5)) {
+            guard let gradientLayer = layer as? CAGradientLayer else { return }
+            gradientLayer.colors = colors.count > 0 ? colors.map({ $0.cgColor }) : nil
+            gradientLayer.startPoint = startPoint
+            gradientLayer.endPoint = endPoint
+            gradientLayer.locations = [0.8, 1.0]
+            
+            guard colors.count > 0 else { return }
+            previousInfo = (colors, startPoint, endPoint)
+        }
+        
+        var previousInfo: (colors: [UIColor], startPoint: CGPoint, endPoint: CGPoint) = ([], .zero, .zero)
+        
+        public func clearGradientColor() {
+            guard let gradientLayer = layer as? CAGradientLayer else { return }
+            gradientLayer.colors = nil
+        }
+        
+        public func restoreGradientColor() {
+            setColors(previousInfo.colors, startPoint: previousInfo.startPoint, endPoint: previousInfo.endPoint)
+        }
+        
+        public override func layoutSubviews() {
+            super.layoutSubviews()
+            
+            guard let gradientLayer = layer as? CAGradientLayer else { return }
+            let fromFloat = Double(min(0.9, 1.0 - (25 / frame.width)))
+            let from = NSNumber(value: fromFloat)
+            gradientLayer.locations = [from, 1.0]
+        }
+    }
+
+}
