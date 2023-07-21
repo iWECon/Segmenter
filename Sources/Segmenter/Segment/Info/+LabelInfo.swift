b7bbe54f3c1736e42a4d7f79a7d7b1120457776b
iWw <i.1214@yahoo.com>
2023-07-21 12:56:08 +0800

    Update and fix some invalid code

--- Sources/Segmenter/Segment/Info/+LabelInfo.swift
+++ Sources/Segmenter/Segment/Info/+LabelInfo.swift
@@ -5,12 +5,12 @@
 import UIKit
 
 public protocol SegmentLabelInfoProvider: SegmentInfoProvider {
-    var title: String? { get }
+    var title: String { get }
     
-    var activeFont: UIFont? { get }
-    var activeColor: UIColor? { get }
-    var inactiveFont: UIFont? { get }
-    var inactiveColor: UIColor? { get }
+    var activeFont: UIFont { get }
+    var activeColor: UIColor { get }
+    var inactiveFont: UIFont { get }
+    var inactiveColor: UIColor { get }
 }
 
 // MARK:- LabelInfo
@@ -21,14 +21,20 @@ extension Segment {
             Label.self
         }
         
-        var title: String?
+        var title: String
         
-        var activeFont: UIFont?
-        var activeColor: UIColor?
-        var inactiveFont: UIFont?
-        var inactiveColor: UIColor?
+        var activeFont: UIFont
+        var activeColor: UIColor
+        var inactiveFont: UIFont
+        var inactiveColor: UIColor
         
-        init(title: String, activeFont: UIFont? = nil, activeColor: UIColor? = nil, inactiveFont: UIFont? = nil, inactiveColor: UIColor? = nil) {
+        init(
+            title: String,
+            activeFont: UIFont = .systemFont(ofSize: 22, weight: .medium),
+            activeColor: UIColor = .darkText,
+            inactiveFont: UIFont = .systemFont(ofSize: 16),
+            inactiveColor: UIColor = .darkGray
+        ) {
             self.title = title
             
             self.activeFont = activeFont
